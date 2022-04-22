4f180bb47f70f23d42da700a07f61a9f2e9f1ce1
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-04-22 16:41:21 +0200

    Rename (snakecase) and small implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Point(i32);
+
+#[derive(Component)]
+struct Territory;
+
+#[derive(Component)]
+struct Tile;
+
+#[derive(Component)]
+struct Character;
+
+fn nature_evolution_system(
+    mut query: Query<&mut Point, With<Territory>>
+){
+    for mut point in query.iter_mut() {
+        point.0 += 1;
+    }
+}
+
+fn setup_scene(
+    mut commands: Commands
+){
+    commands
+        .spawn()
+        .insert(Player)
+        .insert(Territory)
+        .insert(Point(0));
+    
+}
+
+fn print_point_status_system(
+    query: Query<&Point, With<Territory>>
+){
+    for point in query.iter(){
+        println!("point value: {}", point.0);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    App::new()
+        .add_startup_system(setup_scene.system())
+        .add_system(nature_evolution_system.system())
+        .add_system(print_point_status_system.system())
+        .run();
 }

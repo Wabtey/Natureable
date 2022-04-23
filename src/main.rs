b13ded28bd6287a0a618ca22ac071786e756ecf5
Wabtey <73140258+Wabtey@users.noreply.github.com>
2022-04-23 16:35:41 +0200

    Reformed Print system and add Timer to print (once 2s)

--- src/main.rs
+++ src/main.rs
@@ -34,19 +34,34 @@ fn setup_scene(
     
 }
 
+struct PrintTimer(Timer);
+
 fn print_point_status_system(
+    time: Res<Time>, mut timer: ResMut<PrintTimer>,
     query: Query<&Point, With<Territory>>
 ){
-    for point in query.iter(){
-        println!("point value: {}", point.0);
+    if timer.0.tick(time.delta()).just_finished() {
+        for point in query.iter(){
+            println!("point value: {}", point.0);
+        }
+    }
+    
+}
+
+pub struct TestPrint;
+impl Plugin for TestPrint {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(PrintTimer(Timer::from_seconds(2.0, true)))
+            .add_system(print_point_status_system.system());
     }
 }
 
 fn main() {
 
     App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(TestPrint)
         .add_startup_system(setup_scene.system())
         .add_system(nature_evolution_system.system())
-        .add_system(print_point_status_system.system())
         .run();
 }
